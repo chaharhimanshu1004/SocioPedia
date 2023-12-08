150552ab882e4df7da588f79702118b299d121a8
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-08 12:46:36 +0530

    redux setup and themes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@
 
 import {BrowserRouter,Navigate,Route,Routes} from 'react-router-dom'
 import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
 
 
 function App() {
   return (
     <div className="app">
-      hello
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage></LoginPage>}/>
+          <Route path='/home' element={<HomePage></HomePage>}/>
+          <Route path='/profile/:userId' element={<ProfilePage></ProfilePage>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
