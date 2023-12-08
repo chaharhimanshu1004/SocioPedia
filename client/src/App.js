f27bc712223a746be3e0dfd638117fdaade42f69
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-08 12:16:20 +0530

    setup Frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+
+import {BrowserRouter,Navigate,Route,Routes} from 'react-router-dom'
+import HomePage from './scenes/homePage';
+
+
+function App() {
+  return (
+    <div className="app">
+      hello
+    </div>
+  );
+}
+
+export default App;
